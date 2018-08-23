c5985e8f106b7dac0dca60565cb6b86333747437
Thanh Nguyen <thanh.nguyen@playnext-lab.co.jp>
2018-08-23 22:16:13 +0900

    1

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-	
+	if(isset($_POST)) {
+		print_r($_POST);
+	}
 	if(isset($_POST['answers'])) {
 		// $post = json_decode($_POST['answers']);
 		// print_r($post);
-		
 		echo $_POST['answers'];
 	} else {
 		echo json_encode(array("thanh" => "Nguyen", "aaa" => "aaaaaaaaa"));
